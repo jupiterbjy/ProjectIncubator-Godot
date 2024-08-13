c6a22728edae57c89fecbc1eebb7aa11309051eb
jupiterbjy <jupiterbjy@gmail.com>
2024-08-13 16:45:13 +0900

    [Pointcloud] Added pointcloud example

--- PointCloud/Scenes/main.gd
+++ PointCloud/Scenes/main.gd
@@ -1,74 +1,53 @@
 extends Node3D
 
 
-@onready var cam: Camera3D = $Turntable/Camera3D
-@onready var arr_mesh: ArrayMesh = $MeshInstance3D.mesh
+# --- References ---
+
+@onready var arr_mesh: ArrayMesh = %MeshInstance3D.mesh
+
+@onready var lidar_scan_bar: ColorRect = %ColorRect
+
+@onready var point_count_label: Label = %Label
+
+@onready var raycast_scene: Node3D = %RaycastScene
+
+@onready var az_axis: Node3D = $AzimuthTurntable
+@onready var el_axis: Node3D = $AzimuthTurntable/ElevationTurntable
 
 
-var ray_length: float = 100.0
-var ray_mask: int = 1
+# --- Init ---
+
+func _ready() -> void:
+	self.raycast_scene.lidar_cursor = %ColorRect
 
 
-var colors: Array[Color]
-var points: Array[Vector3]
-
-
-## Raycast from given screen position and returns query result
-func raycast_from_screen(pos: Vector2) -> Dictionary:
-	
-	# get ray start & end
-	var from: Vector3 = cam.project_ray_origin(pos)
-	var to: Vector3 = from + cam.project_ray_normal(pos) * self.ray_length
-	
-	# perform raycast
-	var space_state := get_world_3d().direct_space_state
-	
-	var query := PhysicsRayQueryParameters3D.create(from, to, self.ray_mask)
-	query.collide_with_areas = true
-	
-	return space_state.intersect_ray(query)
-
-
-func _physics_process(delta: float) -> void:
-	$Turntable.rotate_y(delta * 0.1)
-	
-	var screen_size: Vector2i = DisplayServer.window_get_size()
-	var rand_screen_pos := Vector2i(
-		randi_range(0, screen_size.x),
-		randi_range(0, screen_size.y),
-	)
-	
-	var result := self.raycast_from_screen(rand_screen_pos)
-	
-	# if not hit ignore
-	if not result:
-		return
-	
-	# else if hit get screen color
-	var color = get_viewport().get_texture().get_image().get_pixelv(rand_screen_pos)
-	
-	# write point
-	self.colors.append(color)
-	self.points.append(result["position"])
+# --- Logic ---
 
 
 func _on_timer_timeout() -> void:
-	# update mesh
+	# on timer, update mesh with new vertices from raycast scene
 	
-	if not self.points:
+	if not self.raycast_scene.points:
 		return
 	
 	var surf_tool := SurfaceTool.new()
 	surf_tool.begin(Mesh.PRIMITIVE_POINTS)
 	
-	for idx: int in range(len(self.points)):
-		surf_tool.set_color(self.colors[idx])
-		surf_tool.add_vertex(self.points[idx])
+	for idx: int in range(len(self.raycast_scene.points)):
+		surf_tool.set_color(self.raycast_scene.colors[idx])
+		surf_tool.add_vertex(self.raycast_scene.points[idx])
 	
+	# SurfaceTool.commit(existing_arr_mesh) creates new 'surface' every call
+	# so need to recreate from scratch
+	self.arr_mesh.clear_surfaces()
 	surf_tool.commit(self.arr_mesh)
 	
-	print("[Main] Added %s verts" % len(self.colors))
+	self.point_count_label.text = "%s / %s points" % [self.raycast_scene.point_count, len(self.raycast_scene.points)]
+
+
+func _physics_process(delta: float) -> void:
 	
-	self.colors.clear()
-	self.points.clear()
-	
+	# rotate object around
+	# WHY THE F X AND Y IS REVERSED??
+	self.az_axis.rotate_x(delta * -0.2)
+	self.el_axis.rotate_y(delta * -0.1)
---
+++ PointCloud/Scenes/raycast_scene.gd
@@ -0,0 +1,114 @@
+extends Node3D
+
+
+# --- References ---
+
+@onready var ray_cam: Camera3D = %RayCamera3D
+
+@onready var az_axis: Node3D = $AzimuthTurntable
+@onready var el_axis: Node3D = $AzimuthTurntable/ElevationTurntable
+
+@onready var viewport: SubViewport = self.get_parent()
+
+# control node to sample screen area from. set this from parent.
+var lidar_cursor: Control = null
+
+# extracted points pos. Use packed arr for speed
+var colors: PackedColorArray
+
+# extracted points color. Use packed arr for speed
+var points: PackedVector3Array
+
+# point count, since we don't want to check how many are in packed array manually
+var point_count: int = 0
+
+# raycast length
+static var _RAY_LENGTH: float = 100.0
+
+# raycast collision mask
+static var _RAY_MASK: int = 1
+
+# scan points count per scan line
+static var POINTS_PER_LINE: int = 100
+
+
+# --- Utility ---
+
+## Raycast from given screen position and returns query result
+func raycast_from_screen(pos: Vector2i) -> Dictionary:
+	
+	# get ray start & end
+	var from: Vector3 = ray_cam.project_ray_origin(pos)
+	var to: Vector3 = from + ray_cam.project_ray_normal(pos) * self._RAY_LENGTH
+	
+	# perform raycast
+	var space_state := get_world_3d().direct_space_state
+	
+	var query := PhysicsRayQueryParameters3D.create(from, to, self._RAY_MASK)
+	query.collide_with_areas = true
+	
+	return space_state.intersect_ray(query)
+
+
+## Get pixel color from given screen position
+func get_screen_color(pos: Vector2i) -> Color:
+	return self.viewport.get_texture().get_image().get_pixelv(pos)
+
+
+# --- Logic ---
+
+## Scan the lidar cursor area and extract point's position and color if any.
+## scanned_perc is in range(0.0~1.0) and represent how many % of points are scanned.
+func scan_area(scanned_perc: float) -> void:
+	
+	# in case you just need random = not sure code is correct now, didn't updated
+	#var rand_screen_pos := Vector2i(
+		#int(self.lidar_scan_bar.global_position.x),
+		#randi_range(0, int(self.lidar_scan_bar.size.y)) + Vector2i(self.lidar_scan_bar.global_position).y,
+	#)
+	
+	# blend between lidar cursor position to get equally spaced scan points
+	var screen_pos: Vector2i = floor(lerp(
+		self.lidar_cursor.position,
+		self.lidar_cursor.position + self.lidar_cursor.size,
+		scanned_perc,
+	))
+	
+	# do raycast and fetch result
+	var result := self.raycast_from_screen(screen_pos)
+	
+	# if not hit ignore
+	if not result:
+		return
+	
+	# else get screen color
+	var color: Color = self.get_screen_color(screen_pos)
+	
+	# append to arr
+	self.colors[self.point_count] = color
+	self.points[self.point_count] = result["position"]
+	self.point_count += 1
+
+
+# --- Driver ---
+
+func _ready() -> void:
+	# prealloctate packedarray
+	self.colors.resize(2048)
+	self.points.resize(2048)
+
+
+func _physics_process(delta: float) -> void:
+	
+	# rotate cam around neko
+	self.az_axis.rotate_y(delta * 0.1)
+	self.el_axis.rotate_x(delta * 0.2)
+	
+	# scan multiple times per line sweep
+	for idx: int in range(POINTS_PER_LINE):
+		self.scan_area(idx / float(POINTS_PER_LINE))
+
+	# if size is in danger expand packedarrays twice of current size
+	if len(self.points) - self.point_count < POINTS_PER_LINE:
+		self.points.resize(len(self.points) * 2)
+		self.colors.resize(len(self.colors) * 2)
