6c9d9e60d1c02f66b35104dcb13bfc93cfef3b4a
jupiterbjy <jupiterbjy@gmail.com>
2024-08-14 02:28:04 +0900

    [Pointcloud] Fixed performance issue, now can run at 60fps

--- PointCloud/Scenes/main.gd
+++ PointCloud/Scenes/main.gd
@@ -1,53 +1,128 @@
 extends Node3D
 
 
+# --- Exports ---
+
+## PackedScene that contains pointcloud mesh.
+## This is to create multile chunks for faster generation.
+@export var point_cloud_scene: PackedScene
+
+
 # --- References ---
 
-@onready var arr_mesh: ArrayMesh = %MeshInstance3D.mesh
+## Displays total points
+@onready var point_count_label: Label = %PointStatLabel
 
-@onready var lidar_scan_bar: ColorRect = %ColorRect
+## Displays points in array
+@onready var arr_stat_label: Label = %ArrayStatLabel
 
-@onready var point_count_label: Label = %Label
+## Displays mesh count
+@onready var mesh_count_label: Label = %MeshStatLabel
 
-@onready var raycast_scene: Node3D = %RaycastScene
+## Subviewport's raycast scene instance for faster acess
+@onready var raycast_scene_instance: RaycastScene = %RaycastScene
 
 @onready var az_axis: Node3D = $AzimuthTurntable
 @onready var el_axis: Node3D = $AzimuthTurntable/ElevationTurntable
+
+## Totall pointcloud mesh count
+var mesh_count: int = 0
+
+## Current actively updated pointcloud's array mesh
+var active_arr_mesh: ArrayMesh = null
+
+## Total cumulative point count
+var total_points: int = 0
+
+## Current active surface tool
+var surf_tool: SurfaceTool = null
 
 
 # --- Init ---
 
 func _ready() -> void:
-	self.raycast_scene.lidar_cursor = %ColorRect
+	self.raycast_scene_instance.lidar_cursor = %ScanCursor
+
+	# add initial pointcloud mesh
+	self.create_new_pointcloud()
+
+
+# --- Utility ---
+
+## Add new pointcloud mesh instance and update references.
+## Originally had mesh seperation on every n points, but removed as there wasn't much perf gain.
+## So this is only used in _ready(). poor lil' thing.
+func create_new_pointcloud() -> void:
+	print("[Main] Creating new pointcloud!")
+
+	var instance: MeshInstance3D = self.point_cloud_scene.instantiate()
+	self.el_axis.add_child(instance)
+
+	# update ref
+	self.active_arr_mesh = instance.mesh
+
+	self.surf_tool = SurfaceTool.new()
+	self.surf_tool.begin(Mesh.PRIMITIVE_POINTS)
+
+	# update counter
+	self.mesh_count += 1
+	%MeshStatLabel.text = "%s mesh(s)" % self.mesh_count
 
 
 # --- Logic ---
 
 
-func _on_timer_timeout() -> void:
-	# on timer, update mesh with new vertices from raycast scene
-	
-	if not self.raycast_scene.points:
-		return
-	
-	var surf_tool := SurfaceTool.new()
-	surf_tool.begin(Mesh.PRIMITIVE_POINTS)
-	
-	for idx: int in range(len(self.raycast_scene.points)):
-		surf_tool.set_color(self.raycast_scene.colors[idx])
-		surf_tool.add_vertex(self.raycast_scene.points[idx])
-	
+
+## Updates pointcloud array mesh.
+func update_pointcloud():
+
+	for idx: int in range(self.raycast_scene_instance.point_count):
+		surf_tool.set_color(self.raycast_scene_instance.colors[idx])
+		surf_tool.add_vertex(self.raycast_scene_instance.points[idx])
+
 	# SurfaceTool.commit(existing_arr_mesh) creates new 'surface' every call
 	# so need to recreate from scratch
-	self.arr_mesh.clear_surfaces()
-	surf_tool.commit(self.arr_mesh)
-	
-	self.point_count_label.text = "%s / %s points" % [self.raycast_scene.point_count, len(self.raycast_scene.points)]
+	self.active_arr_mesh.clear_surfaces()
+	surf_tool.commit(self.active_arr_mesh)
+
+	self.total_points += self.raycast_scene_instance.point_count
+	self.raycast_scene_instance.clear_arr()
 
 
-func _physics_process(delta: float) -> void:
-	
+## Perform scan
+func scan_once() -> void:
+
+	# scan
+	self.raycast_scene_instance.scan_once()
+
+	# update label
+	self.arr_stat_label.text = "%s / %s points in buffer" % [
+		self.raycast_scene_instance.point_count, len(self.raycast_scene_instance.points)
+	]
+	self.point_count_label.text = "%s points total" % (
+		self.total_points + self.raycast_scene_instance.point_count
+	)
+
+
+# --- Driver ---
+
+func _process(delta: float) -> void:
+
 	# rotate object around
-	# WHY THE F X AND Y IS REVERSED??
+	# unlike cam rotation axis is reversed (honestly unintuitive to me why that is)
 	self.az_axis.rotate_x(delta * -0.2)
 	self.el_axis.rotate_y(delta * -0.1)
+
+
+var _tick: bool = true
+
+# Adjust physic frame per second to adjust scan speed.
+func _physics_process(_delta: float) -> void:
+
+	# scan and call pointcloud later
+	if self._tick:
+		self.scan_once()
+	else:
+		self.update_pointcloud()
+
+	self._tick = not self._tick
--- PointCloud/Scenes/raycast_scene.gd
+++ PointCloud/Scenes/raycast_scene.gd
@@ -1,4 +1,18 @@
-extends Node3D
+class_name RaycastScene extends Node3D
+## Raycasting node for use in subviewport.
+
+
+# --- Exports ---
+
+## Collision type. Use convex for simpler shape, trimesh for complex shape.
+@export_enum("Convex", "Trimesh") var collision_type: int
+
+## Scan points per cursor line. Higher the points, worsen the performance.
+## This is due to screen texture fetch cost. (GPU VRAM -> CPU RAM copy)
+@export var points_per_cursor: int = 30
+
+## Max array buffer size.
+@export var arr_size: int = 2 ** 10
 
 
 # --- References ---
@@ -13,10 +27,10 @@ extends Node3D
 # control node to sample screen area from. set this from parent.
 var lidar_cursor: Control = null
 
-# extracted points pos. Use packed arr for speed
+# extracted points pos buffer. Use packed arr for speed
 var colors: PackedColorArray
 
-# extracted points color. Use packed arr for speed
+# extracted points color buffer. Use packed arr for speed
 var points: PackedVector3Array
 
 # point count, since we don't want to check how many are in packed array manually
@@ -28,65 +42,66 @@ static var _RAY_LENGTH: float = 100.0
 # raycast collision mask
 static var _RAY_MASK: int = 1
 
-# scan points count per scan line
-static var _POINTS_PER_CURSOR: int = 20
-
-# starting packedarray size
-static var _ARR_INIT_SIZE: int = 4096
+# cached world 3D
+@onready var world_3d: World3D = get_world_3d()
 
 
 # --- Utility ---
 
 ## Raycast from given screen position and returns query result
 func raycast_from_screen(pos: Vector2i) -> Dictionary:
-	
+
 	# get ray start & end
 	var from: Vector3 = ray_cam.project_ray_origin(pos)
 	var to: Vector3 = from + ray_cam.project_ray_normal(pos) * self._RAY_LENGTH
-	
+
 	# perform raycast
-	var space_state := get_world_3d().direct_space_state
-	
+	var space_state := self.world_3d.direct_space_state
+
 	var query := PhysicsRayQueryParameters3D.create(from, to, self._RAY_MASK)
-	query.collide_with_areas = true
-	
+
 	return space_state.intersect_ray(query)
 
 
-## Get pixel color from given screen position
-func get_screen_color(pos: Vector2i) -> Color:
-	return self.viewport.get_texture().get_image().get_pixelv(pos)
+## Reset array position. Actually just sets item index to 0 without clearing.
+func clear_arr() -> void:
+	self.point_count = 0
+
+
+## Checks if array's almost full
+func is_almost_full() -> bool:
+	var length: int = len(self.points)
+
+	if length - self.point_count < self.points_per_cursor:
+		print("[RaycastScene] Array almost full! (%s / %s)" % [self.point_count, length])
+		return true
+
+	return false
 
 
 # --- Logic ---
 
 ## Scan the lidar cursor area and extract point's position and color if any.
 ## scanned_perc is in range(0.0~1.0) and represent how many % of points are scanned.
-func scan_area(scanned_perc: float) -> void:
-	
-	# in case you just need random = not sure code is correct now, didn't updated
-	#var rand_screen_pos := Vector2i(
-		#int(self.lidar_scan_bar.global_position.x),
-		#randi_range(0, int(self.lidar_scan_bar.size.y)) + Vector2i(self.lidar_scan_bar.global_position).y,
-	#)
-	
+func scan_area(scanned_perc: float, screen_tex: Texture2D) -> void:
+
 	# blend between lidar cursor position to get equally spaced scan points
 	var screen_pos: Vector2i = floor(lerp(
 		self.lidar_cursor.position,
 		self.lidar_cursor.position + self.lidar_cursor.size,
 		scanned_perc,
 	))
-	
+
 	# do raycast and fetch result
 	var result := self.raycast_from_screen(screen_pos)
-	
+
 	# if not hit ignore
 	if not result:
 		return
-	
+
 	# else get screen color
-	var color: Color = self.get_screen_color(screen_pos)
-	
+	var color: Color = screen_tex.get_image().get_pixelv(screen_pos)
+
 	# append to arr
 	self.colors[self.point_count] = color
 	self.points[self.point_count] = result["position"]
@@ -97,26 +112,33 @@ func scan_area(scanned_perc: float) -> void:
 
 func _ready() -> void:
 	# prealloctate packedarray
-	self.colors.resize(self._ARR_INIT_SIZE)
-	self.points.resize(self._ARR_INIT_SIZE)
-	
-	# TODO: create collision shape in code
+	self.colors.resize(self.arr_size)
+	self.points.resize(self.arr_size)
+
+	# generate static collision shape from mesh so we don't have to press in GUI
+	# when changing meshes
+	if self.collision_type == 0:
+		%MeshInstance3D.create_convex_collision()
+	elif self.collision_type == 1:
+		%MeshInstance3D.create_trimesh_collision()
 
 
-func _physics_process(delta: float) -> void:
-	
+func _process(delta: float) -> void:
+
 	# rotate cam around neko
 	self.az_axis.rotate_y(delta * 0.1)
 	self.el_axis.rotate_x(delta * 0.2)
-	
-	# scan multiple times per line sweep
-	for idx: int in range(_POINTS_PER_CURSOR):
-		self.scan_area(idx / float(_POINTS_PER_CURSOR))
 
-	# if size is in danger expand packedarrays twice of current size
-	var length: int = len(self.points)
-	
-	if length - self.point_count < _POINTS_PER_CURSOR:
-		print("[RaycastScene] Resizing array! (%s → %s)" % [length, length * 2])
-		self.points.resize(length * 2)
-		self.colors.resize(length * 2)
+
+var next_v_pos: int = 0
+
+
+## Scans at scan cursor position once.
+func scan_once():
+
+	# fetch screen texture here to save GPU-CPU IO
+	var screen_tex: Texture2D = self.viewport.get_texture()
+
+	# scan multiple times per line sweep
+	for idx: int in range(self.points_per_cursor):
+		self.scan_area(idx / float(self.points_per_cursor), screen_tex)
